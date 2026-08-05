a8b54c548dbf6e35a7f1b6f72d87d179a7c2e093
ardirpl17-dot <ardirpl17@gmail.com>
2026-08-05 08:37:27 +0700

    ardiwoii

---
+++ program pertama .py
@@ -0,0 +1,25 @@
+# program pertamaku
+# nama: [ardi-nihbos]
+#kelas: X RPL
+
+
+print("============================")
+print("SELAMAT DATANG DI PYTHON")
+print("============================")
+print()
+print("program ini dibuat oleh ardi:","[ardicahbagussaksmpn1ken]")
+print("Sekolah: SMK TJP")
+print("Tahun ajaran",2026,"-",2028)
+print(10+5)
+print(4*7)
+print(2**8)
+print(10/2)
+print(10%3)
+print(10//3)
+print("senin","selasa")
+print("senin","selasa",sep=" | ")
+print("selasa",end="!")
+a=27
+b=4
+print(a+b)
+print(a-b)
